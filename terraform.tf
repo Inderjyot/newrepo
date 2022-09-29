002ca006dca43a8ff4a3c53279ad0c0ca9a7d31c
Inderjyot Singh <61014300+Inderjyot@users.noreply.github.com>
2022-09-29 15:31:40 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web" {
     ami = "ami-026b57f3c383c2eec"
     instance_type = "t2.micro"
     count = 1
-    key_name = "jenkins-key"
+    key_name = "jenkin-key"
     tags = {
       "Name" = "web"
     }
